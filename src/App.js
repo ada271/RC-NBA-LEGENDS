d4a0fcacbbde5cde5efc1f032f18dcf40a1e14e2
ada <h.kaansolmaz@gmail.com>
2024-07-12 10:53:01 +0300

    cc

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { Container } from "react-bootstrap";
-import "./App.css";
+
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
+import CardContainer from "./components/CardContainer";
 
 function App() {
   return (
     <div className="App">
       <Container className="text-center">
         <Header/>
+        <CardContainer/>
 
       </Container>
     </div>
---
+++ src/components/CardContainer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import {data} from "../helper/data"
+
+const CardContainer = () => {
+
+    // console.log(data)
+  return (
+    <>
+    {/* INPUT acilacak */}
+    
+    
+    </>
+  )
+}
+
+export default CardContainer
