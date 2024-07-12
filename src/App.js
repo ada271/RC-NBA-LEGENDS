dbd81a8a823c04972c45ac07feb60aa85012a1c5
ada <h.kaansolmaz@gmail.com>
2024-07-12 10:31:31 +0300

    hdr

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Container } from "react-bootstrap";
 import "./App.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      
+      <Container className="text-center">
+        <Header/>
+
+      </Container>
     </div>
   );
 }
