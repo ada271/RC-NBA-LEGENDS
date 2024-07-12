4c1e2108483f2d5ccb83f5903cdc10650f8cd2cf
ada <h.kaansolmaz@gmail.com>
2024-07-12 12:50:15 +0300

    bb

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { data } from "../helper/data";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Form, Row } from "react-bootstrap";
 import Player from "./Player";
 
 const CardContainer = () => {
     // console.log(data)
+
+    const [arama, setArama] = useState("");
+
+    let filtrelenmisData = "";
+    filtrelenmisData = data.filter((a) => a.name.includes(arama));
+    console.log(filtrelenmisData)
     return (
         <>
             {/* INPUT acilacak */}
 
+            <Form.Control
+                className="w-50 m-auto"
+                type="search"
+                placeholder="Search Player..."
+                onChange={(e) => setArama(e.target.value)}
+            />
+
             <Container className="card-container my-4 p-3 rounded-4">
                 <Row className="g-3">
-                    {data.map((player) => (
+                    {filtrelenmisData.map((player) => (
                         <Col md={6} lg={4} xl={3} key={player.name}>
-                            <Player {...player}/>
+                            <Player {...player} />
                         </Col>
                     ))}
                 </Row>
